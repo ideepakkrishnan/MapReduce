e54b37c0e823ec32f7e7df040be2daf427be37aa
ideepakkrishnan <ideepakkrishnan@outlook.com>
2017-02-20 23:51:25 -0500

    Updating the pre processing mapper

--- pageRank/src/main/java/com/neu/pdp/pageRank/preProcessor/TokenizerMapper.java
+++ pageRank/src/main/java/com/neu/pdp/pageRank/preProcessor/TokenizerMapper.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -19,6 +20,17 @@ import org.apache.hadoop.mapreduce.Mapper.Context;
  * @author ideepakkrishnan
  */
 public class TokenizerMapper extends Mapper<Object, Text, Text, Text> {
+	
+	// Class level variables
+	private static Pattern namePattern;
+	private static Pattern linkPattern;
+	
+	static {
+		// Keep only html pages not containing tilde (~).
+		namePattern = Pattern.compile("^([^~]+)$");
+		// Keep only html filenames ending relative paths and not containing tilde (~).
+		linkPattern = Pattern.compile("^\\..*/([^~]+)\\.html$");
+	}
 
 	/**
 	 * Reads data from a compressed bz2 file and extracts
@@ -37,7 +49,17 @@ public class TokenizerMapper extends Mapper<Object, Text, Text, Text> {
             ) throws IOException, InterruptedException {
 		StringTokenizer itr = new StringTokenizer(value.toString(), "\n");
 		while (itr.hasMoreTokens()) {
-			System.out.println(itr.nextToken());
+			String line = itr.nextToken();
+			
+			// Get the delimiter
+			int delimLoc = line.indexOf(':');
+			
+			// Split and get the page name and corresponding html
+			String pageName = line.substring(0, delimLoc);
+			String html = line.substring(delimLoc + 1);
+			
+			
+			
 		}
 	}
 }
