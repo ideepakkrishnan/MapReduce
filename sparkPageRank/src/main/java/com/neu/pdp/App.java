06607182a0123f570b7a5583bcca0b8900e85215
ideepakkrishnan <ideepakkrishnan@outlook.com>
2017-03-02 23:19:43 -0500

    Testing word count in spark

--- sparkPageRank/src/main/java/com/neu/pdp/App.java
+++ sparkPageRank/src/main/java/com/neu/pdp/App.java
@@ -1,5 +1,8 @@
 package com.neu.pdp;
 
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Word Count" );
+        JavaRDD<String> textFile = sc.textFile("hdfs://...");
+        JavaPairRDD<String, Integer> counts = textFile
+            .flatMap(s -> Arrays.asList(s.split(" ")).iterator())
+            .mapToPair(word -> new Tuple2<>(word, 1))
+            .reduceByKey((a, b) -> a + b);
+        counts.saveAsTextFile("hdfs://...");
     }
 }
